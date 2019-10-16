a83376db527d0a51a0c59619222495d0504ec28d
albiton <52779534+albiton@users.noreply.github.com>
2019-10-16 22:46:15 +0600

    Update printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -1,24 +1,23 @@
 package piscine
 
-import "fmt"
+import (
+        "github.com/01-edu/z01"
+)
 
 func PrintComb() {
-         
-	j:=1
-	k:=2
-	for i := 0; i <= 7; i++ {
-		for j = i + 1; j <= 8; j++ {
-			for k = j + 1; k <= 9; k++ {       
-				fmt.Print(i)
-				fmt.Print(j)
-				fmt.Print(k)
-				if i < 7 {
-				fmt.Print(", ")
-				}else {
-				    fmt.Print("\n")
+	
+        for a := '0'; a <= '9'; a++ {
+			for b := a+1; b <= '9'; b++ {
+				for c := b+1; c <= '9'; c++ {
+					z01.PrintRune(a)
+					z01.PrintRune(b)
+					z01.PrintRune(c)
+					if a < 55 {
+					z01.PrintRune(44)
+					z01.PrintRune(32)
+					}
 				}
 			}
 		}
-	}
-	
-} 
+	z01.PrintRune('\n')
+}
