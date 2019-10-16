42b0a0dc29a581f8c0bfb9067f0132e55e610a72
albiton <52779534+albiton@users.noreply.github.com>
2019-10-16 22:41:48 +0600

    Update printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -1,28 +1,24 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import "fmt"
 
 func PrintComb() {
-  var n rune
-  var m rune
-  var o rune
-  for n = 0; n <= 9; n++ {
-    for m = 0; m <= 9; m++ {
-      if m != n && m > n {
-        for o = 0; o <= 9; o++ {
-          if o != m && o > m {
-            z01.PrintRune(n + 48)
-            z01.PrintRune(m + 48)
-            z01.PrintRune(o + 48)
-            if !(n == 7 && m == 8 && o == 9) {
-              z01.PrintRune(',')
-              z01.PrintRune(' ')
-            }
-          }
-        }
-      }
-    }
-  }
-  z01.PrintRune(10)
-}
-
+         
+	j:=1
+	k:=2
+	for i := 0; i <= 7; i++ {
+		for j = i + 1; j <= 8; j++ {
+			for k = j + 1; k <= 9; k++ {       
+				fmt.Print(i)
+				fmt.Print(j)
+				fmt.Print(k)
+				if i < 7 {
+				fmt.Print(", ")
+				}else {
+				    fmt.Print("\n")
+				}
+			}
+		}
+	}
+	
+} 
