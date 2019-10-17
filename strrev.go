ef6c01c5f04e0043fab68c7997c042365e1edec8
albiton <52779534+albiton@users.noreply.github.com>
2019-10-17 22:09:30 +0600

    Update strrev.go

--- strrev.go
+++ strrev.go
@@ -1,12 +1,8 @@
 package piscine
 
-<<<<<<< HEAD
-func StrRev(s string) string {
-=======
 func StrRev (s string) string {
->>>>>>> 48ea4de6a0e622e5c693c1dd28ce2b5848038185
 	var reverse string
-	for i, j := 0, len(s)-1; i > 0; i-- {
+	for i := 0, len(s)-1; i > 0; i-- {
 		reverse += string(s[i])
 	}
 	return reverse
