9486f0eb923bff22e3ff7177b30d216be8862b6a
albiton <albitowka@gmail.com>
2019-10-17 12:13:13 +0600

    kdjfojjf

--- printcomb.go
+++ printcomb.go
@@ -1,29 +1,24 @@
-package main
+package piscine
 
-import  "github.com/01-edu/z01"
+import "fmt"
 
-func main() {
-
-	a := 1
-	b := 2
-	for a := '0'; a <= '7'; a++ {
-		for b = a + 1; b <= 8; b++ {
-			for c = b + 1 ; c <= 9; b++
-			if a < b && b < c {
-               	if a == '7' && b == '8' && c == '9' {
-				   z01.PrintRune(a)
-				   z01.PrintRune(b)
-				   z01.PrintRune(c)
-               	} else {
-               		z01.PrintRune(a)
-               		z01.PrintRune(b)
-               		z01.PrintRune(c)
-               		z01.PrintRune(',')
-               		z01.PrintRune(' ')
-               	}
-            }
-        }
-    }			 
- }
- z01.PrintRune (10)
- }							
+func PrintComb() {
+         
+	j:=1
+	k:=2
+	for i := 0; i <= 7; i++ {
+		for j = i + 1; j <= 8; j++ {
+			for k = j + 1; k <= 9; k++ {       
+				fmt.Print(i)
+				fmt.Print(j)
+				fmt.Print(k)
+				if i < 7 {
+				fmt.Print(", ")
+				}else {
+				    fmt.Print("\n")
+				}
+			}
+		}
+	}
+	
+} 
