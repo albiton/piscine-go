b30a2d48dc44ef9e169e94830a0b8fce9d791c94
albiton <52779534+albiton@users.noreply.github.com>
2019-10-17 17:56:48 +0600

    Update printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -6,9 +6,9 @@ func PrintComb() {
 
 	b := 1
 	c := 2
-	for a  := 0; a <= 7; a++ {
-		for b  = b + 1; b <= 8; b++ {
-			for c  = c + 1; c <= 9; c++ {
+	for a := 0; a <= 7; a++ {
+		for b = b + 1; b <= 8; b++ {
+			for c = c + 1; c <= 9; c++ {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(c)
