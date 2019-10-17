24f8054d856e7a1cf2011157eb51c8df46f4f5fb
albiton <albitowka@gmail.com>
2019-10-17 18:59:47 +0600

    fgfvhsfhsfjvh

--- printcomb.go
+++ printcomb.go
@@ -1,14 +1,13 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-
-	b := 1
-	c := 2
-	for a := 0; a <= 7; a++ {
-		for b = b + 1; b <= 8; b++ {
-			for c = c + 1; c <= 9; c++ {
+	b := '1'
+	c := '2'
+	for a := '0'; a <= '7'; a++ {
+		for b = a + 1; b <= '8'; b++ {
+			for c = b + 1; c <= '9'; c++ {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(c)
@@ -19,4 +18,8 @@ func PrintComb() {
 			}
 		}
 	}
+}
+
+func main() {
+	PrintComb()
 }
