e15874b86450a00e2c4e7767ad100307edcecc30
albiton <52779534+albiton@users.noreply.github.com>
2019-10-21 13:53:51 +0600

    Update iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,8 @@
 package piscine
 
-func IterativeFactorial(nb int) int {
-	if nb < 0 {
-		return 0
-	if nb == 0  && nb < 20 {
+ffunc IterativeFactorial(nb int) int {
+	if nb == 0 {
 		return 1
 	}
 	return nb * IterativeFactorial(nb-1)
-	}
 }
