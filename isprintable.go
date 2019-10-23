c3f988a1b548976d1f50fe3caa7f358bc4d5ff77
albiton <52779534+albiton@users.noreply.github.com>
2019-10-23 17:22:52 +0600

    Create isprintable.go

---
+++ isprintable.go
@@ -0,0 +1,18 @@
+package main
+
+func IsPrintable(str string) bool {
+	if str == "" {
+		return false
+	}
+	s := []rune(str)
+	for _, letter := range s {
+		if letter >= 'a' && letter <= 'z' {
+			continue
+		} else if letter >= 'A' && letter <= 'Z' {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
