65bcc1ae21eb00708d7c0296ca5bb4dd489b4777
albiton <52779534+albiton@users.noreply.github.com>
2019-10-24 23:09:26 +0600

    Create makerange.go

---
+++ makerange.go
@@ -0,0 +1,16 @@
+package piscine
+
+func MakeRange(min, max int) []int {
+	if max <= min {
+		return nil
+	}
+
+	lice := make([]int, max-min)
+	index := 0
+	for k := range lice {
+		lice[k] = min + index
+		index++
+	}
+	return lice
+
+}
