8af9ccdb64ae100d7896417f5a0fbaf2f150fdc6
albiton <52779534+albiton@users.noreply.github.com>
2019-10-24 22:53:06 +0600

    appendrange

---
+++ appendrange.go
@@ -0,0 +1,14 @@
+package student
+
+func AppendRange(min, max int) []int {
+	var slice []int
+	for i := min; i < max; i++ {
+
+		slice = append(slice, i)
+	}
+	if max <= min {
+		return nil
+	}
+	return slice
+
+}
