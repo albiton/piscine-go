265bef6eed78d8fdda5ca9dca81aadfef118de49
albiton <52779534+albiton@users.noreply.github.com>
2019-10-24 20:55:10 +0600

    Update recursivepower.go

--- quest04/recursivepower.go
+++ quest04/recursivepower.go
@@ -6,8 +6,8 @@ func RecursivePower(nb int, power int) int {
 	}
 	if power == 0 {
 		return 1
+	} else {
+		result = nb * RecursivePower(nb, power-1)
 	}
-	res := 1
-	res = nb * RecursivePower(nb, power-1)
-	return res
+	return result
 }
