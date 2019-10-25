70cf103834d33a91ea9ed269409f20c7b2f7871a
albiton <52779534+albiton@users.noreply.github.com>
2019-10-25 13:16:53 +0600

    Update isprime.go

--- quest04/isprime.go
+++ quest04/isprime.go
@@ -1,10 +1,7 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb == 1 {
-		return false
-	}
-	if nb <= 0 {
+	if nb == 1 || nb <= 0 {
 		return false
 	} else {
 		for i := 2; i*i <= nb; i++ {
